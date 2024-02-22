4734e118f5b0f59994bc82e85b7bee6ede18a289
Ishan Bhargava <ishanbhargava@gmail.com>
2024-02-22 11:39:10 -0500

    Updated number of teams to 19

--- src/Progress.js
+++ src/Progress.js
@@ -10,7 +10,7 @@ function Progress() {
 
     const [prog, setProg] = useState([]);
     const progressCollectionRef = collection(db, "progress");
-    const ar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    const ar = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
     const week = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
     const getProg = async () => {
